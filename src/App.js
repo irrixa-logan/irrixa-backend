c04ecd559cee3f39776dc186553fe437dafbddfa
Logan Chislett <logan@irrixa.com.au>
2025-04-25 14:12:27 +1000

    Add Firebase Auth + Login flow

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./AuthContext";
+import ProtectedRoute from "./ProtectedRoute";
+import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import "./index.css";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<Login />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
+  );
 }
 
 export default App;
