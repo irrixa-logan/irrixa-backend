8a52a81783884c3f3993f97e757b828702bbad68
Logan Chislett <logan@irrixa.com.au>
2025-05-18 18:00:40 +1000

    Fix: allow public Flask port binding

--- server.py
+++ server.py
@@ -19,4 +19,7 @@ def get_irrigation_results():
         return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
